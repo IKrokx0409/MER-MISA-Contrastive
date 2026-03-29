bf17418788b255d25499edca583288da89caf17b
nscscc2025-058 <syr2737267@163.com>
2026-03-29 19:41:04 +0800

    bert-tokenizer

--- src/data_loader.py
+++ src/data_loader.py
@@ -80,7 +80,7 @@ def get_loader(config, shuffle=True):
         for sample in batch:
             text = " ".join(sample[0][3])
             encoded_bert_sent = bert_tokenizer(
-                text, max_length=SENT_LEN+2, add_special_tokens=True, pad_to_max_length=True)
+                text, max_length=SENT_LEN+2, add_special_tokens=True, padding='max_length', truncation=True)
             bert_details.append(encoded_bert_sent)
             # delete encode_plus
 
