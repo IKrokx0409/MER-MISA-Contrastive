c8ae5bcff95dfab64340de1cdf647551e73efa2c
nscscc2025-058 <syr2737267@163.com>
2026-03-30 03:26:54 +0800

    fix sqrt

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -96,7 +96,7 @@ class CMD(nn.Module):
     def matchnorm(self, x1, x2):
         power = torch.pow(x1-x2,2)
         summed = torch.sum(power)
-        sqrt = summed**(0.5)
+        sqrt = (summed + 1e-6)**(0.5)
         return sqrt
         # return ((x1-x2)**2).sum().sqrt()
 
